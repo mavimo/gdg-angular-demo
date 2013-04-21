bcb9271f5c6d8e90cf8b349177f60cbaae13e5b2
mavimo <mavimo@gmail.com>
2013-04-21 17:42:56 +0200

    Changed testing mode to singleRun and increased timeout

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -50,8 +50,8 @@ autoWatch = false;
 browsers = ['Chrome'];
 
 // If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+captureTimeout = 10000;
 
 // Continuous Integration mode
 // if true, it capture browsers, run tests and exit
-singleRun = false;
+singleRun = true;
